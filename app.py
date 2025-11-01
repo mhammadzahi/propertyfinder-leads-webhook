231e117f422326c4d6a7ff2a1c7994019eaf21a2
Mohamed Zahi <mohamedzahi33@gmail.com>
2025-11-01 13:20:34 +0400

    pull listing

--- app.py
+++ app.py
@@ -16,27 +16,31 @@ api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
 
 
-lead_csv_file = "propertyfinder_leads_10K__2__.csv"
-for j in range(1, 201):
+
+listing_ids = get_column_values("propertyfinder_leads_10K__2__.csv", "listing.id")
+i = 2
+for listing_id in listing_ids:
     access_token = get_propertyfinder_token(api_key, api_secret)
-    data, pagination = get_leads(access_token, j, 50)
+    data = get_listing_by_id(access_token, listing_id.strip())
+    save_to_csv(data, "listings_with_location_10K__2__.csv")
+    if data:
+        generate_listing_by_id("listing_by_id_10K__2__.csv", listing_id, i, json.dumps(data))# Natalia Mapping
+        i += 1
 
-    save_to_csv(data, lead_csv_file)
-    download_all_from_csv(csv_file=lead_csv_file, url_column="call.recordFile", save_dir="recordings", force_download=False):
-
-    print(pagination)
+    else:
+        print(f"No Data for listing id: {listing_id}")
 
 
 
 
-# listing_ids = get_column_values("propertyfinder_leads_10K.csv", "listing.id")
-# i = 2
-# for listing_id in listing_ids:
+# Leads 10K
+# lead_csv_file = "propertyfinder_leads_10K__2__.csv"
+# for j in range(1, 201):
 #     access_token = get_propertyfinder_token(api_key, api_secret)
-#     data = get_listing_by_id(access_token, listing_id.strip())
-#     save_to_csv(data, "listings_with_location_3.csv")
-#     if data:
-#         generate_listing_by_id("listing_by_id_3.csv", listing_id, i, json.dumps(data))
-#         i += 1
-#     else:
-#         print(f"No Data for listing id: {listing_id}")
+#     data, pagination = get_leads(access_token, j, 50)
+
+#     save_to_csv(data, lead_csv_file)
+#     download_all_from_csv(csv_file=lead_csv_file, url_column="call.recordFile", save_dir="recordings", force_download=False)
+
+#     print(pagination)
+
